d35588ff802f5c93c2d432c45725410b6dbbac4c
Matt Turner <matturner@gmail.com>
2019-07-04 17:53:27 -0400

    Add kernel heap & allocator that uses it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 #![cfg_attr(not(test), no_std)]
 
+extern crate alloc;
+
 // re-export these
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
 pub mod serial;
 pub mod vga;
+
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("Allocation error for: {:?}", layout)
+}
 
 pub fn sleep_loop() -> ! {
     loop {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+extern crate alloc;
+
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
@@ -14,6 +17,7 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     interrupts::init();
     let mut mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
     let mut frame_allocator = memory::BootInfoFrameAllocator_new(&boot_info.memory_map);
+    allocator::init(&mut mapper, &mut frame_allocator).expect("Heap initialisation failed");
 
     use x86_64::structures::paging::{Page, PhysFrame};
     use x86_64::{PhysAddr, VirtAddr};
@@ -27,6 +31,21 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     println!("Welcome to mTOS.");
     serial_println!("Hello Host!");
+
+    let x = Box::new(42);
+    println!("value on the heap: {} at {:p}", x, x);
+
+    let mut v = Vec::new();
+    for i in 0..69 {
+        v.push(i);
+    }
+    println!("vec at {:p}; item 42: {}", v.as_slice(), v[42]);
+
+    let rc = Rc::new(vec![0,1,2]);
+    let clone = rc.clone();
+    println!("current ref count is {}", Rc::strong_count(&clone));
+    core::mem::drop(rc);
+    println!("current ref count is {}", Rc::strong_count(&clone));
 
     //unsafe { exit_qemu() };
     mtos::sleep_loop();
